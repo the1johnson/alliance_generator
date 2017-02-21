1b8daab4ab52a55e7d60c7725057c8d508b206ab
John Johnson <johnj@booj.com>
2017-02-21 12:43:28 -0700

    Initial Build

---
+++ scripts/config_live.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_HOST', 'the1johnsoncom.fatcowmysql.com');
+    define('DB_USER', 'the1johnson');
+    define('DB_PASS', '!Password1');
+    define('DB_NAME', 'alliance');
+    define('DB_TYPE', 'mysql');
+?>
